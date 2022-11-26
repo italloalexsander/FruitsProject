fb6ec5b56f0d426168b9c342468443152b45e2ea
italloalexsander <italloalexsander@gmail.com>
2022-11-26 15:01:20 -0300

    Add insertion of documents

---
+++ app.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url= 'mongodb://localhost:27017';
+
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url);
+
+client.connect(function(err){
+  assert.equal(null, err);
+  console.log("Connected Successfully");
+
+  const db = client.db(dbName);
+  client.close();
+});
+
+const insertDocuments = function(db, callback){
+  const collection = db.collection('fruits');
+  collection.insertMany([
+    {
+      name: "Apple",
+      score: 8,
+      review: "Great fruit"
+    },
+    {
+      name: "Orange",
+      score: 6,
+      review: "Kinda sour"
+    },
+    {
+      name: "Banana",
+      score: 9,
+      result: "Great Stuff!"
+    }
+  ], function(err, result){
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  })
+}

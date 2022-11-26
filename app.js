b9cec210975a2e0abce14f776ee86d1b305a6dcf
italloalexsander <italloalexsander@gmail.com>
2022-11-26 15:38:33 -0300

    Start changing to mongoose

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
+
+
+
 
 const url= 'mongodb://localhost:27017';
 

3750795dc3b32278def1882479ea45160033c819
italloalexsander <italloalexsander@gmail.com>
2022-11-26 15:56:46 -0300

    Add new insertion method for mongoose

--- app.js
+++ app.js
@@ -2,60 +2,53 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-
-
-
-const url= 'mongodb://localhost:27017';
-
-const dbName = 'fruitsDB';
-
-const client = new MongoClient(url);
-
-client.connect(function(err){
-  assert.equal(null, err);
-  console.log("Connected Successfully");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, () => {
-    findDocuments(db, () => {
-      client.close();})
-    })
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
 });
 
-const insertDocuments = function(db, callback){
-  const collection = db.collection('fruits');
-  collection.insertMany([
-    {
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      result: "Great Stuff!"
-    }
-  ], function(err, result){
-    assert.equal(err, null);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  })
-}
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const findDocuments = function(db, callback){
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 8,
+  review: "Sweet enough."
+});
 
-  const collection = db.collection('fruits');
+//fruit.save();
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  gender: String
+});
 
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  })
-}
+const Person = mongoose.model("Person", personSchema)
+
+const person = new Person({
+  name: "John",
+  age: 25,
+  gender: "Male"
+});
+
+//person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 9,
+  review: "Pretty good!"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 10,
+  review: "Actually the best fruit!"
+});
+
+Fruit.insertMany([kiwi, orange], function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Operation was succesfull")
+  }
+})
